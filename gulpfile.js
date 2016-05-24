b629b6cac84f40f8e7b654e424d03d637c04f3c9
Stefan Huber <stefan.huber@beyondit.at>
2016-05-24 14:00:27 +0200

    updated typings to newest version

--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,7 @@ gulp.task('watch', ['clean'], function(done){
     function(){
       gulpWatch('app/**/*.scss', function(){ gulp.start('sass'); });
       gulpWatch('app/**/*.html', function(){ gulp.start('html'); });
-      buildBrowserify({ watch: true }).on('end', done);
+      buildBrowserify({ watch: true , src: ['./app/app.ts', './typings/index.d.ts'] }).on('end', done);
     }
   );
 });
