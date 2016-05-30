1dbea2664fd8961b89cf0f0f699ce3c03baa1eb2
Roland Schellhorn <roland.schellhorn@beyondit.at>
2016-05-30 14:52:44 +0200

    styling minor changes

--- app/pages/payment/payment-result.ts
+++ app/pages/payment/payment-result.ts
@@ -1,35 +1,73 @@
+import {ChangeDetectorRef} from '@angular/core';
 import {Page,NavParams,NavController} from 'ionic-angular';
 import {AmountPage} from '../amount/amount';
-import {NgClass} from '@angular/common';
+import {Transaction} from '../../api/transaction';
+import {Config} from '../../providers/config';
+import {Currency} from '../../providers/currency/currency';
+import * as payment from '../../providers/payment/payment';
+import {BitcoinUnit} from '../../providers/currency/bitcoin-unit';
+
+let PAYMENT_STATUS_MESSAGES = {};
+PAYMENT_STATUS_MESSAGES[payment.PAYMENT_STATUS_TIMEOUT]  = 'Payment request timed out, please start again';
+PAYMENT_STATUS_MESSAGES[payment.PAYMENT_STATUS_RECEIVED] = 'Your payment was successfully processed';
+PAYMENT_STATUS_MESSAGES[payment.PAYMENT_STATUS_SERVICE_ERROR] = 'Bitcoin Network currently not accessable';
+PAYMENT_STATUS_MESSAGES[payment.PAYMENT_STATUS_NOT_RECEIVED] = 'Payment not received';
+PAYMENT_STATUS_MESSAGES[payment.PAYMENT_STATUS_ERROR] = 'Payment error';
 
 @Page({
-    templateUrl : 'build/pages/payment/payment.html',
-    directives: [NgClass]
+    templateUrl : 'build/pages/payment/payment-result.html'
 })
-
 export class PaymentResultPage {
     resultIcon : string = "";
     resultClass = { "transaction-success" : false , "transaction-failed" : true };
     resultText : string = "";
     success : boolean = false;
-    
-    constructor(private params: NavParams, private nav: NavController) {
+    transaction:Transaction;
 
-        this.success = params.data.status !== false;
+    currency:string;
+    fiatAmount:string;
+    bitcoinAmount:string;
+    bitcoinUnit:string;
+
+    getResultClasses() {
+        return this.resultClass;
+    }
+
+    constructor(private currencyService: Currency, private config: Config, private params: NavParams, private nav: NavController, private changeDetector: ChangeDetectorRef) {
+        this.success = (params.data.success === true);
+        this.transaction = params.data.transaction;
+
+        if (PAYMENT_STATUS_MESSAGES[params.data.status]) {
+            this.resultText = PAYMENT_STATUS_MESSAGES[params.data.status];
+        } else {
+            this.resultText = PAYMENT_STATUS_MESSAGES[payment.PAYMENT_STATUS_ERROR];
+        }
 
         if (this.success) {
             this.resultClass["transaction-success" ] = true;
-            this.resultClass["transaction-failed" ] = false;
+            this.resultClass["transaction-failed" ]  = false;
             this.resultIcon = "checkmark-circle";
         } else {
             this.resultClass["transaction-success" ] = false;
-            this.resultClass["transaction-failed" ] = true;
+            this.resultClass["transaction-failed" ]  = true;
             this.resultIcon = "close-circle";
         }
-        
+
+        Promise.all<any>([
+            this.config.get('bitcoin-unit') ,
+            this.config.get('currency-format-s')
+        ]).then(promised => {
+            this.bitcoinUnit = promised[0];
+            this.currency = this.transaction.currency;
+            this.bitcoinAmount = this.currencyService.formatNumber(BitcoinUnit.from(this.transaction.bitcoinAmount,'BTC').to(this.bitcoinUnit), promised[1]);
+            this.fiatAmount = this.currencyService.formatNumber(this.transaction.fiatAmount, promised[1]);
+
+            this.changeDetector.detectChanges();
+        })
+
         setTimeout(() => {
             nav.setRoot(AmountPage);
-        }, 5000);                           
+        }, 5000);
     }
-    
+
 }
