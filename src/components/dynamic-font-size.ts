de0bb2b5fb8b8f5763875ce207ada06178dce871
Stefan Huber <stefan.huber@beyondit.at>
2016-12-26 22:57:23 +0100

    hacky solution to font size problem

--- src/components/dynamic-font-size.ts
+++ src/components/dynamic-font-size.ts
@@ -2,27 +2,40 @@ import { Directive, ElementRef, Input, Renderer } from '@angular/core';
 
 @Directive({ selector: '[dynamicFontSize]' })
 export class DynamicFontSize {
-    
-    @Input() dynamicText: any;    
-    @Input() breakPoint: any;
-    @Input() maxSize: any;
-    
-    maxFontSize = 0;
+        
+    @Input() maxFontSize:any;
+    @Input() minFontSize:any;    
+    @Input() content:string = "";
 
     constructor(private el: ElementRef, private renderer: Renderer) {
+        this.el.nativeElement.style.fontSize = this.maxFontSize + "px";
     }
 
-    ngOnChanges() {
-        /*
-        if (this.el && this.el.parentElement && this.el.parentElement.parentElement) {
-            this.maxFontSize = this.el.parentElement.parentElement.clientHeight * (this.maxSize ? 1 : 0.55);            
-        }
-        let textLength = this.dynamicText.length;
-        let shrinkValue = this.maxFontSize * (1/this.breakPoint) * (textLength-this.breakPoint);
+    calculateFontSize() {
+        let childWidth     = this.el.nativeElement.clientWidth;
+        let parentWidth    = this.el.nativeElement.parentElement.clientWidth; 
+        let fontSize       = parseInt(this.el.nativeElement.style.fontSize) || parseInt(this.maxFontSize);
+        let calculatedSize = (fontSize * ((parentWidth/childWidth)-0.1));
 
-        if (this.maxFontSize > 0) {            
-            this.el.style.fontSize = (textLength > this.breakPoint ? (this.maxFontSize - shrinkValue) : this.maxFontSize) + 'px';
+        if (calculatedSize < parseInt(this.maxFontSize) && calculatedSize > parseInt(this.minFontSize)) {
+            this.el.nativeElement.style.fontSize = calculatedSize + "px";
+            childWidth = this.el.nativeElement.clientWidth + 20;
+
+            if (childWidth >= parentWidth) {
+                setTimeout(() => {
+                    this.calculateFontSize();
+                },0);                             
+            }
         }
-        */
+    }
+
+    ngAfterViewInit() {      
+        setTimeout(() => {
+            this.calculateFontSize();
+        },50);          
+    }    
+    
+    ngOnChanges() {
+        this.calculateFontSize();
     }
 }
