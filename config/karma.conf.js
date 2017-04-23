51cbb27661289d90ff29f4a0a7faf52fe7d4d190
Stefan Huber <stefan.huber@beyondit.at>
2017-04-23 18:35:21 +0200

    chore: changed from phantomjs to firefox

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -35,7 +35,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS'],
+    browsers: ['Firefox'],
     singleRun: true
   };
 
